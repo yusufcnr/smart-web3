879437411424c51176efc353eadb5cd36e17d1ac
Yusuf Cinar <75760010+yusufcnr@users.noreply.github.com>
2022-03-24 15:21:13 +0300

    Update GetAndSet.sol

--- GetAndSet.sol
+++ GetAndSet.sol
@@ -1,1 +1,31 @@
 
+//SPDX-License-Identifier:MIT
+pragma solidity^0.8.0;
+
+contract ReadWrite {
+
+
+    string public myString = "first string";
+
+    function readData() public view returns (string memory) {
+
+        return myString;
+    }
+
+    function writeData(string memory _myString) public {
+
+        
+        myString = _myString;
+
+    }
+
+
+    function sub (uint8 a, uint8 b) public pure returns (uint) {
+
+        return a - b;
+    } 
+
+}
+
+
+
